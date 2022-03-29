546e5a6021196abb8357871a9057e6fbe1e83d2e
KaitoTakase <k-takase@vectorinc.co.jp>
2022-03-29 13:49:27 +0900

    add: TheHomeArrow.vue
    矢印のコンポーネント追加

---
+++ components/home/TheHomeArrow.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="mv__arrow-wrap">
+        <div class="mv__arrow">
+            <p class="mv__arrow-text">→</p>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+@import '~/assets/styles/project/home/_mv';
+</style>
